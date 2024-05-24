edcf98a71165c599ba1b5da63c1f0d0e6108de50
faizan-ashraf <faizanashraf0021@gmail.com>
2024-05-24 21:15:15 +0500

    live chats clickable

--- src/app/components/live-chat/live-chat.component.ts
+++ src/app/components/live-chat/live-chat.component.ts
@@ -22,12 +22,12 @@ export class LiveChatComponent implements OnInit{
   contactName: any = 'Lalaen Sultan';
   constactSubject: any = 'Datastructure and Algorithm - Lab';
   constact : any[] = [
-    { name: 'Lalaen Sultan' , lastMessage: '' , lastSeen: new Date() , imageUrl: '/assets/bot1.png' , chatId: 1},
-    { name: 'Dr Nabeel Sabir Khan' , lastMessage: '' , lastSeen: new Date() , imageUrl: '/assets/bot2.jpg' , chatId: 2 },
-    { name: 'Nadia Urooj' , lastMessage: '' , lastSeen: new Date() , imageUrl: '/assets/bot3.jpg' , chatId: 3 },
-    { name: 'Saira Latif' , lastMessage: '' , lastSeen: new Date() , imageUrl: '/assets/bot4.webp' , chatId: 4 },
-    { name: 'Aasma Abdul Waheed' , lastMessage: '' , lastSeen: new Date() , imageUrl: '/assets/bot5.png' , chatId: 5 },
-    { name: 'Aurooj Butt' , lastMessage: '' , lastSeen: new Date() , imageUrl: '/assets/bot6.jpg' , chatId: 6 },
+    { name: 'Lalaen Sultan' , lastMessage: '' , lastSeen: new Date() , imageUrl: '/assets/bot1.png' , chatId: 1 , subject:'Datastructure and Algorithms - Lab' },
+    { name: 'Dr Nabeel Sabir Khan' , lastMessage: '' , lastSeen: new Date() , imageUrl: '/assets/bot2.jpg' , chatId: 2 , subject:'Datastructure and Algorithms' },
+    { name: 'Nadia Urooj' , lastMessage: '' , lastSeen: new Date() , imageUrl: '/assets/bot3.jpg' , chatId: 3 , subject:'English III' },
+    { name: 'Saira Latif' , lastMessage: '' , lastSeen: new Date() , imageUrl: '/assets/bot4.webp' , chatId: 4 , subject: 'Software Requirement Engineering' },
+    { name: 'Aasma Abdul Waheed' , lastMessage: '' , lastSeen: new Date() , imageUrl: '/assets/bot5.png' , chatId: 5 , subject: 'Web Engineering' },
+    { name: 'Aurooj Butt' , lastMessage: '' , lastSeen: new Date() , imageUrl: '/assets/bot6.jpg' , chatId: 6 , subject: 'Probability and Statistics' },
   ]
 
   constructor(
@@ -39,6 +39,13 @@ export class LiveChatComponent implements OnInit{
   ngOnInit(): void {
     this.subscription = this.loginService.actionObservable.subscribe(action => {
       this.tableWidth = action;
-  });
+    });
+  }
+  selectContact(contactId : any){
+    debugger
+    const contact = this.constact.find(e=> e.chatId == contactId);
+    this.chatUrl = contact?.imageUrl
+    this.contactName = contact?.name;
+    this.constactSubject = contact?.subject;
   }
 }
